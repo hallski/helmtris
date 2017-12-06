1235726962953754076a1e52cada26907591c283
Mikael Hallendal <hallski@hallski.org>
2017-12-06 10:18:32 +0100

    Support moving the block left/right
    No bounds checking yet though.

--- src/Main.elm
+++ src/Main.elm
@@ -2,58 +2,68 @@ module Main exposing (main)
 
 import Color exposing (..)
 import Html exposing (..)
+import Html.Events exposing (onClick, on, keyCode)
+import Char exposing (KeyCode)
 import Collage
 import Transform exposing (..)
 import Element
-import Dict exposing (..)
-
+import Time
+import AnimationFrame as AF
+import Json.Decode as Json
+import Keyboard as KB
 
 gridSize : Int
 gridSize = 25
+
+playFieldSize : { cols : Int, rows : Int}
 playFieldSize = { cols = 10, rows = 20 }
+
+playFieldDimensions : { width : Int, height : Int}
 playFieldDimensions =
     { width = playFieldSize.cols * gridSize
     , height = playFieldSize.rows * gridSize
     }
 
-f c x = (x, c)
-
 iBlock : Grid
 iBlock =
-    [ List.map (f 0) <| List.range 0 3 ]
+    [ List.map (flip (,) <| 0) <| List.range 0 3 ]
 
+jBlock : Grid
 jBlock =
-    [ List.map (f 1) <| List.range 0 2
+    [ List.map (flip (,) <| 0) <| List.range 0 2
     , [ (2, 1) ]
     ]
 
+lBlock : Grid
 lBlock =
-    [ List.map (f 2) <| List.range 0 2
+    [ List.map (flip (,) <| 0) <| List.range 0 2
     , [ (0, 2) ]
     ]
 
+oBlock : Grid
 oBlock =
     [ [ (0, 3), (1, 3) ]
     , [ (0, 3), (1, 3) ]
     ]
 
+sBlock : Grid
 sBlock =
     [ [ (1, 4), (2, 4) ]
     , [ (0, 4), (1, 4) ]
     ]
 
+tBlock : Grid
 tBlock =
-    [ List.map (f 5) <| List.range 0 2
+    [ List.map (flip (,) <| 0) <| List.range 0 2
     , [ (1, 5) ]
     ]
 
+zBlock : Grid
 zBlock =
     [ [ (0, 6), (1, 6) ]
     , [ (1, 6), (2, 6) ]
     ]
 
-
-type Msg = NoOp
 
 type alias Position = (Int, Int)
 
@@ -61,6 +71,7 @@ type alias Position = (Int, Int)
 type alias Cell = (Int, Int)
 
 type alias Grid = List (List Cell)
+type alias Block = (Position, Grid)
 
 emptyGrid : Grid
 emptyGrid =
@@ -77,19 +88,61 @@ demoGrid =
 
 
 type alias Model =
-    { landed : Grid
-    , dropping : (Position, Grid)
+    { playing : Bool
+    , landed : Grid
+    , nextDrop : Time.Time
+    , dropping : Block
     }
 
 
 init : (Model, Cmd Msg)
 init =
-    ( Model demoGrid <| ((3, 0), iBlock), Cmd.none )
+    ( Model False demoGrid 0 <| ((3, 0), iBlock), Cmd.none )
+
+
+type Msg
+    = NoOp
+    | Tick Time.Time
+    | TogglePlay
+    | Left
+    | Right
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Tick time ->
+            ( updateDropping model time, Cmd.none )
+
+        TogglePlay ->
+            ( { model | playing = not model.playing }, Cmd.none )
+
+        Left ->
+            ( { model | dropping = moveDropping -1 model.dropping }, Cmd.none )
+
+        Right ->
+            ( { model | dropping = moveDropping 1 model.dropping }, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
+
+
+moveDropping : Int -> Block -> Block
+moveDropping dx ((x, y), g) =
+    ((x + dx, y), g)
+
+
+updateDropping : Model -> Time.Time -> Model
+updateDropping model time =
+    if time < model.nextDrop then
+        model
+    else
+        let
+            ((x, y), g) = model.dropping
+            newBlock = ((x, y + 1), g)
+            -- Check collision
+        in
+            { model | dropping = newBlock, nextDrop = time + Time.second }
 
 
 getColor : Int -> Color
@@ -103,6 +156,7 @@ getColor shapeId =
         5 -> brown
         6 -> purple
         _ -> white
+
 
 renderLine : List Cell -> List Collage.Form -> List Collage.Form
 renderLine line rendered =
@@ -119,7 +173,7 @@ renderLine line rendered =
                 renderLine rest <| renderedCell :: rendered
 
 
-renderLines : Float -> Float -> Grid -> List Collage.Form ->  List Collage.Form
+renderLines : Float -> Float -> Grid -> List Collage.Form -> List Collage.Form
 renderLines xOffset yOffset lines rendered =
     case lines of
         [] ->
@@ -131,7 +185,7 @@ renderLines xOffset yOffset lines rendered =
                     renderLine line []
                         |> Collage.groupTransform (Transform.translation xOffset yOffset)
             in
-                renderLines xOffset (yOffset - (toFloat gridSize)) rest <| renderedLine :: rendered
+                renderLines xOffset (yOffset + (toFloat gridSize)) rest <| renderedLine :: rendered
 
 
 renderGrid : Float -> Float -> Grid -> Collage.Form
@@ -139,28 +193,59 @@ renderGrid xOffset yOffset grid =
     renderLines xOffset yOffset grid []
         |> Collage.group
 
-startX =
-    -(toFloat (playFieldDimensions.width - gridSize)) / 2
 
-startY =
-    (toFloat (playFieldDimensions.height - gridSize)) / 2
+{--
+  Simplify the rendering code by applying a transformation on all forms drawn.
+
+  Since Collage in elm-graphics puts origin in the center with Y growing upwards and draws all
+  forms anchored to the center as well, flip the Y axis and translate origin to be half a gridSize
+  in from the top left.
+--}
+canvasTranslation : Transform.Transform
+canvasTranslation =
+    let
+        startX = -(toFloat (playFieldDimensions.width - gridSize)) / 2
+        startY = -(toFloat (playFieldDimensions.height - gridSize)) / 2
+    in
+        Transform.multiply (Transform.scaleY -1) (Transform.translation startX startY)
 
 
 view : Model -> Html Msg
 view model =
     let
+        togglePlayStr = if model.playing then "Pause" else "Start"
+    in
+        div [ ]
+            [ viewPlayField model
+            , button [ onClick TogglePlay ] [ text togglePlayStr ]
+            ]
+
+
+handleKey : KeyCode -> Msg
+handleKey code =
+    case code of
+        65 ->
+            Left
+        68 ->
+            Right
+        _ ->
+            NoOp
+
+
+viewPlayField : Model -> Html Msg
+viewPlayField model =
+    let
         ((x, y), g) = model.dropping
 
-        _ = model.dropping |> Debug.log("Meh")
-
-        xOffset = startX + (toFloat (x * gridSize))
-        yOffset = startY - (toFloat (y * gridSize))
-        forms = [ renderGrid startX startY model.landed
+        xOffset = (toFloat (x * gridSize))
+        yOffset = (toFloat (y * gridSize))
+        forms = [ renderGrid 0 0 model.landed
                 , renderGrid xOffset yOffset g
-                , Collage.rect 25 25 |> Collage.filled brown |> Collage.move (-100, startY)
+                , Collage.rect 25 25 |> Collage.filled darkBlue |> Collage.move (0, 0)
                 ]
     in
-        Collage.collage playFieldDimensions.width playFieldDimensions.height forms
+        Collage.collage playFieldDimensions.width playFieldDimensions.height
+            [ Collage.groupTransform canvasTranslation forms ]
             |> Element.color gray
             |> Element.toHtml
 
@@ -176,4 +261,10 @@ main =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    if model.playing then
+        Sub.batch
+            [ AF.times Tick
+            , KB.downs handleKey
+            ]
+    else
+        Sub.none
